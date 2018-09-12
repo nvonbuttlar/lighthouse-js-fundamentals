4ba7db3a6fb3f13076931f0b9997776cae9c98e9
vonbutt <nick.von.buttlar@gmail.com>
2018-09-12 05:55:58 +0000

    Adding hello.js and even.js documents

---
+++ hello.js
@@ -0,0 +1,14 @@
+function sayHello (name) {
+  console.log("Hello ", name);
+}
+
+sayHello("Frau");
+sayHello("Scott");
+sayHello("Number 2");
+
+/* This kind of function produces a side effect,
+as in it does something. The other main type is a function that
+produces a result, as in it calculates and returns a value we can use
+in further code. One such function could be for determining whether
+a number is even. Hint: Let's do that next. */
+
