1fe0098b198093f4f82c4e0165554004e891c7b4
vonbutt <nick.von.buttlar@gmail.com>
2018-09-14 05:33:51 +0000

    Fizz Buzz type exercise

---
+++ loopylighthouse1.js
@@ -0,0 +1,14 @@
+var i = 0;
+for (i = 100; i <= 200; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (i % 3 === 0) {
+    console.log("Loopy");
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
+}
+
+// This is the game FIZZ BUZZ with the numbers and words changed.
